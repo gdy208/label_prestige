fc53f8d70bcec0987301b9f07f25cf5bbf4a6f99
Gdy <gedeonkouame208@gmail.com>
2026-06-17 04:11:16 +0000

    Fix base path pour GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
+const base = process.env.BASE_URL || '/';
+
 export default defineConfig({
+  base,
   plugins: [tailwindcss()],
 });
